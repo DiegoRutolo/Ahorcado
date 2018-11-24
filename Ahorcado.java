4e85f408630ba2a5571e46f8295435c1366c4878
DiegoRutolo <diegorutolo@protonmail.com>
2018-11-24 13:20:08 +0100

    Juego básico terminado

--- Ahorcado.java
+++ Ahorcado.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 class Palabra {
 	String palabra;
-	char[] letras;
+	char[] letras = new char[0];
+	int fallos = 0;
 
 	Palabra (String s) {
 		this.palabra = s;
@@ -14,31 +15,72 @@ class Palabra {
 		System.arraycopy(this.letras, 0, antLet, 0, this.letras.length);
 		antLet[antLet.length-1] = c;
 		this.letras = antLet;
+
+		boolean contiene = false;
+		for (int i = 0; i < this.palabra.length(); i++) {
+			if (c == this.palabra.charAt(i)) {
+				contiene = true;
+				break;
+			}
+		}
+		if (!contiene) {
+			this.fallos++;
+		}
 	}
 
 	String getSecreto() {
 		String secreto = "";
 		for (int i = 0; i < this.palabra.length(); i++) {
-			if (new String(this.letras).contais(this.palabra.charAt(i))) {
-				secreto = secreto + this.palabra.charAt(i);
-			} else {
-				secreto = secreto + "*";
+			boolean contiene = false;
+			for (char c : this.letras) {
+				if (c == this.palabra.charAt(i)) {
+					contiene = true;
+					break;
+				}
 			}
+			secreto = secreto + (contiene ? this.palabra.charAt(i) : "*");
 		}
 		return secreto;
+	}
+
+	boolean estaCompleta() {
+		for (int i = 0; i < this.palabra.length(); i++) {
+			boolean contiene = false;
+			for (char c : this.letras) {
+				if (c == this.palabra.charAt(i)) {
+					contiene = true;
+					break;
+				}
+			}
+			if (!contiene) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
 
 public class Ahorcado {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Random rnd = new Random();	
+		Random rnd = new Random();
 		String[] frutas = {
-			"manzana", "naranja", "melon", "platano","sandia", "kiwi"
+			"manzana", "sandia", "kiwi"
 		};
 		Palabra palabra = new Palabra(frutas[rnd.nextInt(frutas.length)]);
 		System.out.println("Adivina la palabra.");
 		System.out.println("El tema es frutas: ");
-		System.out.println(palabra.getSecreto());
+		System.out.println("···················\n");
+		do {
+			System.out.println(palabra.getSecreto());
+			if (palabra.estaCompleta()) {
+				System.out.println("GANASTE :D");
+				return;
+			}
+			System.out.print("Fallos: " + palabra.fallos + " -> ");
+			palabra.addLetra(sc.next().charAt(0));
+			System.out.print("\n");
+		} while (palabra.fallos < 10);
+		System.out.println("Demasiados fallos. Perdiste :-\\");
 	}
 }
